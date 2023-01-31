ebc000a37ce4848fbd5d4840700e4d2c9bb228da
alejandrogonzalz <alex.aga816@gmail.com>
2023-01-31 01:42:32 -0600

    31 de enero 1:42 am

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
 console.log('Hello everybody'); 
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
